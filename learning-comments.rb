bd9a71c0ea3dbfddfe3ea37abee1f0e89563df73
kalada William-jumbo <wkalada1@gmail.com>
2021-02-19 18:54:44 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,7 @@
+puts ( 1 + 1)
+
+#from the three dog night song: "joy to the world (jeremiah was a bullfrog"
+puts "joy to the world"
+puts "all the boys and girls"
+#puts "joy to athe fishes in the deep blue sea"
+puts "joy to you and me"
